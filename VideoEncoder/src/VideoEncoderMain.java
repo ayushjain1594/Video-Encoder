ce9f02ced7dfc36ce0dfef13193e9bff9639199a
Sanya <skalra@bu.edu>
2017-12-01 23:48:58 +0530

    Added Command line

--- VideoEncoder/src/VideoEncoderMain.java
+++ VideoEncoder/src/VideoEncoderMain.java
@@ -7,27 +7,61 @@ import java.util.TreeMap;
 
 public class VideoEncoderMain {
 
-    public static void main(String [] arg) {
-        BufferedImage [] imgOut;
-        VideoPlayer v;
+    public static int epsilon = 100;
+    public static void printMenu()
+    {
+        System.out.println("To encode, give following arguments: encode [file1] [file2] ... [fileN] --output [outputFile]");
+        System.out.println("To view, give following arguments: view [outputFile]");
+    }
+
+    public static void encode(String[] filenames, String outputFileName){
         VSegment vSeg = new VSegment();
         ArrayList<TreeMap<Byte,Byte>> arr;
+
         String loc=System.getProperty("user.dir")+"\\Images"; //Location where the images are stored.
-        VFrame [] frames = new VFrame[100];
+        VFrame [] frames = new VFrame[filenames.length];
         try {
-            for (int i = 0; i < 100; i++) {
-                frames[i] = new VFrame(ImageIO.read(new File(loc + String.format("\\dimage%03d.jpg", i))), i);
+            for (int i = 0; i < filenames.length; i++) {
+                frames[i] = new VFrame(ImageIO.read(new File(loc + String.format("\\" + filenames[i], i))), i);
             }
             vSeg.addFrames(frames);
-            arr = vSeg.compressSegment("cvideo.sar", 100);
-            imgOut = vSeg.decompressSegment("cvideo.sar");
-            v = new VideoPlayer(imgOut,10);
-            v.playVideo();
+            arr = vSeg.compressSegment(outputFileName + ".sar", epsilon);
         }
         catch(IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void decode(String outputFileName){
+        VSegment vSeg = new VSegment();
+        BufferedImage [] imgOut;
+        VideoPlayer v;
+        imgOut = vSeg.decompressSegment(outputFileName + ".sar");
+        v = new VideoPlayer(imgOut,10);
+        v.playVideo();
+    }
+    public static void main(String [] arg) {//throws IOException {
 
+        if(arg.length < 2)
+            printMenu();
+        else if (arg[0].equals("encode"))
+        {
+            String[] filenames = new String[arg.length-3];
+            String outputFilename;
+            int end = arg.length - 3;
+            for (int i = 0; i < end; i++)
+            {
+                filenames[i] = arg[i + 1];
+            }
+            outputFilename = arg[arg.length-1];
+            encode(filenames, outputFilename);
+        }
+        else if (arg[0].equals("view"))
+        {
+            decode(arg[1]);
+        }
+        else
+            printMenu();
+
+    }
 }
